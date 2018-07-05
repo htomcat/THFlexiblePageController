559d0c1113b5bf92f8bb903a69c5b245b0d9f1c2
htomcat <gllu--u.com@hotmail.co.jp>
2018-07-05 10:06:58 +0900

    [add] UIViewController's extension

---
+++ THFlexiblePageController/THFlexiblePageController/Extensions/UIViewController+CustomUpperTab.swift
@@ -0,0 +1,13 @@
+//
+//  UIViewController+CustomUpperTab.swift
+//  THFlexiblePageController
+//
+//  Created by 浜田　智史 on 2018/07/05.
+//  Copyright © 2018年 htomcat. All rights reserved.
+//
+
+import Foundation
+
+extension UIViewController {
+    
+}
