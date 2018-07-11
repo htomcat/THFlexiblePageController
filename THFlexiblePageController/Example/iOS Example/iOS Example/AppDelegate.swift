1c1129f230458f9b30362b66784aba2526949f96
htomcat <gllu--u.com@hotmail.co.jp>
2018-07-11 10:02:30 +0900

    [modify] Example

--- THFlexiblePageController/Example/iOS Example/iOS Example/AppDelegate.swift
+++ THFlexiblePageController/Example/iOS Example/iOS Example/AppDelegate.swift
@@ -21,17 +21,30 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         test1.view.backgroundColor = .red
         test1.title = "test1"
         test1.upperTabsColor = .red
+
         let test2 = UIViewController()
         test2.view.backgroundColor = .yellow
         test2.title = "test2"
         test2.upperTabsColor = .yellow
+
         let test3 = UIViewController()
         test3.view.backgroundColor = .blue
         test3.title = "test3"
         test3.upperTabsColor = .blue
+
+        let test4 = UIViewController()
+        test4.view.backgroundColor = .green
+        test4.title = "test4"
+        test4.upperTabsColor = .green
+
+        let test5 = UIViewController()
+        test5.view.backgroundColor = .black
+        test5.title = "test5"
+        test5.upperTabsColor = .black
+
         let root = RootViewController.makeInstance()
-        let config = Configuration(tabsHeight: 60, tabsWidth: 150, selectedLineColor: .gray, backgroundColor: .white, hiddenSeparatedLine: false)
-        root.setPages([test1, test2, test3], config: config)
+        let config = Configuration(tabsHeight: 30, tabsWidth: 100, selectedLineColor: .gray, backgroundColor: .white, hiddenSeparatedLine: true, spacing: 0)
+        root.setPages([test1, test2, test3, test4, test5], config: config)
         window?.rootViewController = root
         window?.makeKeyAndVisible()
         return true
